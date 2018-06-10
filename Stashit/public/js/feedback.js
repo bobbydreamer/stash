ed5b28c7623347349bc8e8a8edb10e7708999045
Sushanth Bobby Lloyds <bobby.dreamer@gmail.com>
2018-06-11 00:14:20 +0530

    Updated feedback to use callable functions to Feedbacks project

--- Stashit/public/js/feedback.js
+++ Stashit/public/js/feedback.js
@@ -6,6 +6,25 @@ document.addEventListener('DOMContentLoaded', event => {
     const btnSubmit = document.getElementById('btnSubmit');
     const alert = document.getElementById('alert');
 
+    //Setting up firebase secondary project
+    var secondaryAppConfig = {
+        apiKey: "AIzaSyBQDPMJIVM8d3sYAw2FuLoOuvQOHjfWebc",
+        authDomain: "feedbacks-9be7f.firebaseapp.com",
+        databaseURL: "https://feedbacks-9be7f.firebaseio.com",
+        projectId: "feedbacks-9be7f"
+    };
+
+    // Initialize another app with a different config
+    var secondary = firebase.initializeApp(secondaryAppConfig, "secondary");
+
+    // Retrieve the database.
+    var secondaryDatabase = secondary.database();
+
+    function feedbacks(){
+        var appListRef = secondaryDatabase.ref().child('ApplicationList');
+        return appListRef.once('value').then(snap => snap.val());        
+    }
+
     //function
     function getName(firebaseUser){
         var userNameRef = firebase.database().ref().child('users/' + firebaseUser.uid +'/name');
@@ -42,55 +61,85 @@ document.addEventListener('DOMContentLoaded', event => {
                 errMsg = "no text in message box";
                 reject('Missing value : '+errMsg);
             }          
-            resolve('OK');   
+            // resolve('OK');   
+            resolve(prepareData());
         });
     }
 
-    btnSubmit.addEventListener('click', e => {
-        //txtStatus.innerHTML
-        validateForm().then( (resolved) => {
-            //console.log('validate form = ',resolved);
-            // Firebase References
-            var ref = firebase.database().ref().child('feedbacks');
-            var stashRef = ref.child('stash');
+    function getYearWeek(unixts) {
+        date = new Date(unixts); 
+        var onejan = new Date(date.getFullYear(), 0, 1);
+        return date.getFullYear()+''+Math.ceil((((date - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+    }
 
-            const auth = firebase.auth();
+    function prepareData(){
+        const auth = firebase.auth();
 
-            var updates = {};
-            const uid = auth.currentUser.uid; //var userId = firebase.auth().currentUser.uid;    
-            let page = 'Stash';
-            let name = txtName.value;            
-            let email = txtEmail.value;
-            let comment = txtMessage.value.replace(/\n/g, "<br />");     
-            let dateSaved = Date.now();
+        var updates = {};
+        const uid = auth.currentUser.uid; //var userId = firebase.auth().currentUser.uid;    
+        let subject = 'Stash - Feedback from '+uid;
+        let name = txtName.value;            
+        let email = txtEmail.value;
+        let application = 'stash';
+        let message = txtMessage.value.replace(/\n/g, "<br />");     
+        let dateSaved = Date.now();
+        let status = 'active', category='none';
+        // console.log(uid, page, name, email, comment, dateSaved);            
+        var addFeedback = { subject, name, email, application, message, dateSaved, status, category };
+
+        return addFeedback;
+    }
     
-            // console.log(uid, page, name, email, comment, dateSaved);    
-            var addFeedback = { uid, page, name, email, comment, dateSaved };
-                    
-            // Add the new Stash
-            var key = stashRef.push().key;
-            // console.log('key = ',key);
-            // console.log(addFeedback);
-            updates['/feedbacks/' + page + '/' + key] = addFeedback;            
-            firebase.database().ref().update(updates);            
+    btnSubmit.addEventListener('click', e => {
+        validateForm().then( (resolved) => {
+            // console.log('validate form feedback() = ',resolved);
+            let application = resolved.application;
+            let yearweek = getYearWeek(Date.now());
+            let data = {
+                feedback:resolved,
+                yearweek,
+                application
+            };
 
-            // Show alert
-            document.querySelector('.alert').style.display = 'block';
-            alert.innerHTML = 'Thank you for your feedback';
+            // console.log('Before writeFeedback() - ',data);
+            var writeFeedback = secondary.functions().httpsCallable('writeFeedback');
+            writeFeedback(data).then( (result) => {
+                // console.log('Result : ',result);
+                if(result.data.status){
+                    // Show alert
+                    document.querySelector('.alert').style.display = 'block';
+                    alert.innerHTML = 'Thank you for your feedback';
 
-            // Hide alert after 3 seconds
-            setTimeout(function(){
-                document.querySelector('.alert').style.display = 'none';	
-            },3000);
-            
+                    // Hide alert after 3 seconds
+                    setTimeout(function(){
+                        document.querySelector('.alert').style.display = 'none';	
+                    },30000);
+                }else{
+                    //txtStatus.innerHTML= error;
+                    document.querySelector('.alert').style.display = 'block';
+                    alert.innerHTML = 'Sorry! Not able to send your feedback';
+                }                
+            }).catch( (error) => { 
+                var code = error.code;
+                var message = error.message;
+                var details = error.details;
+                if(code != undefined){
+                    document.querySelector('.alert').style.display = 'block';                    
+                    let temp = 'Sorry! Not able to send your feedback. CODE:'+code+' MESSAGE:'+message+' DETAILS:'+details;
+                    console.log(temp);    
+                    alert.innerHTML = temp;
+                }                                                
+            });
+                
         }).catch( (error) => {
             //txtStatus.innerHTML= error;
             document.querySelector('.alert').style.display = 'block';
             alert.innerHTML = 'Error : ' + error;
         });
-        
+            
     });
-    //---------------------------------
+
+
 
     // Check AUTH state change   
     firebase.auth().onAuthStateChanged(firebaseUser => {
